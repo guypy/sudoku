e1ee1641189bc0776fdb18fb56ba520b2b383660
Guy Pinchuk <guypinchuk@gmail.com>
2018-05-12 16:09:49 +0300

    created clion project

---
+++ cmake-build-debug/CMakeFiles/sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudoku.dir/main.c.o"
+  "sudoku.pdb"
+  "sudoku"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
