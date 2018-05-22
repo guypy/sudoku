afaca647e018793cdbe39b5e98a6df37a82dc99d
Guy Pinchuk <guypinchuk@gmail.com>
2018-05-22 23:27:49 +0300

    wip

--- parser.c
+++ parser.c
@@ -33,7 +33,8 @@ char* prsr_fetchCmd(int *args){
     while (cmdpt == NULL) {
         fgets(cmd_str, 1024, stdin);
         cmdpt = strtok(cmd_str, " \t\r\n");
-        if (feof(stdin) != 0 && cmdpt == NULL) {
+        if (feof(stdin)) {
+            printf("%s", cmdpt);
             strcpy(cmd, EXIT);
             return cmd;
         }
